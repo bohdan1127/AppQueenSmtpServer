6711899768943e12b0025409aeb25a80bcd1f73f
Ruhul Amin <swopnoo@yahoo.com>
2020-03-26 11:57:28 +0600

    Update server.js
        disabledCommands: ['STARTTLS'],

--- smtp/server.js
+++ smtp/server.js
@@ -46,7 +46,8 @@ const server = new SMTPServer({
     
     // not required but nice-to-have
     banner: 'Welcome to My Awesome SMTP Server',
-
+    
+    disabledCommands: ['STARTTLS'],
     // disable STARTTLS to allow authentication in clear text mode
     //disabledCommands: ['AUTH', 'STARTTLS'],
 
